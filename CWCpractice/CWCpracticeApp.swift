f39aecf47eb0426f1eeb0367acd3ce5429a3323e
KyuLaf <sks5075@naver.com>
2021-12-23 19:43:17 +0900

    Initial Commit

---
+++ CWCpractice/CWCpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWCpracticeApp.swift
+//  CWCpractice
+//
+//  Created by 송규섭 on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CWCpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
